9d21b3cae5ceab937b099f47f42dde11436c31a3
Cayke Manjanelli Xavier <cayke@hotmail.com>
2022-01-09 17:39:13 -0300

    firt Class: Criando primeira classe de teste

---
+++ src/test/java/br/com/alura/tdd/modelo/FuncionarioTest.java
@@ -0,0 +1,20 @@
+package br.com.alura.tdd.modelo;
+
+import org.junit.jupiter.api.Test;
+
+public class FuncionarioTest {
+    @Test
+    void testGetDataAdmissao() {
+
+    }
+
+    @Test
+    void testGetNome() {
+
+    }
+
+    @Test
+    void testGetSalario() {
+
+    }
+}
---
+++ src/test/java/br/com/alura/tdd/service/BonusServiceTest.java
@@ -0,0 +1,41 @@
+package br.com.alura.tdd.service;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import org.junit.jupiter.api.Test;
+
+import br.com.alura.tdd.modelo.Funcionario;
+
+public class BonusServiceTest {
+    
+    @Test
+    void BonusZeroParaSalarioMaiorQue10000() {
+
+        BonusService bonus = new BonusService();
+        var valorBonus = bonus.calcularBonus(new Funcionario("Jose", LocalDate.now(), new BigDecimal("20000")));
+
+        assertEquals(new BigDecimal("0.00"), valorBonus);
+    }
+
+    @Test
+    void BonusDe10PorCentoParaSalarioAte10000() {
+
+        BonusService bonus = new BonusService();
+        var valorBonus = bonus.calcularBonus(new Funcionario("Jose", LocalDate.now(), new BigDecimal("2500")));
+
+        assertEquals(new BigDecimal("250.00"), valorBonus);
+    }
+
+    @Test
+    void BonusDe10PorCentoParaSalarioigual10000() {
+
+        BonusService bonus = new BonusService();
+        var valorBonus = bonus.calcularBonus(new Funcionario("Jose", LocalDate.now(), new BigDecimal("10000")));
+
+        assertEquals(new BigDecimal("1000.00"), valorBonus);
+    }
+
+}
